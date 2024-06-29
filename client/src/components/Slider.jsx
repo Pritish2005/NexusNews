fb085c88b35baa5955fcbdb3faeb89fc83670e87
revantbhatia <bhatiarevant@gmail.com>
2024-06-29 17:31:08 +0530

    minor UI fix

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -6,13 +6,13 @@ function Slider({ modeData }) {
   return (
     <div className='flex w-full h-4 px-12'>
       <div className='bg-blue-500 flex items-center justify-center' style={{ width: `${modeData.left}%`, minHeight: '1rem' }}>
-        {modeData.left > 10 ? `${modeData.left}%` : ''}
+        {modeData.left > 10 ? `${Math.ceil(modeData.left)-1}%` : ''}
       </div>
       <div className='bg-slate-300 flex items-center justify-center' style={{ width: `${modeData.center}%`, minHeight: '1rem' }}>
-        {modeData.center > 10 ? `${modeData.center}%` : ''}
+        {modeData.center > 10 ? `${Math.ceil(modeData.center)-1}%` : ''}
       </div>
       <div className='bg-red-500 flex items-center justify-center' style={{ width: `${modeData.right}%`, minHeight: '1rem' }}>
-        {modeData.right > 10 ? `${modeData.right}%` : ''}
+        {modeData.right > 10 ? `${Math.ceil(modeData.right)-1}%` : ''}
       </div>
     </div>
   );
